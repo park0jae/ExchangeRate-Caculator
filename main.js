cd3edf05a029b4bc8d39b9b8661580408dff84ce
park0jae <84398970+park0jae@users.noreply.github.com>
2022-01-28 23:04:27 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 'use strict';
-const startRate = document.querySelector(".start-rate .rate_list");
-const endRate = document.querySelector(".end-rate .rate_list");
-const startBtn = document.querySelector(".start-rate .fas");
-const endBtn = document.querySelector(".end-rate .fas");
+const startRate = document.querySelector(".start-rate .rate .rate-title .main");
+const endRate = document.querySelector(".end-rate .rate .rate-title .main");
+const startBtn = document.querySelector(".start-rate .rate .fas");
+const endBtn = document.querySelector(".end-rate .rate .fas");
 
 console.log(startRate);
+console.log(startBtn);
+
 
 startBtn.addEventListener("click" , ()=>{
     startRate.classList.toggle("click");
